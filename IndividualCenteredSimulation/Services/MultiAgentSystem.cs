40a2d202eb47c4c6b28af548c13e98620cecf8d3
Junior Buléon <juniorburleon@hotmail.com>
2017-01-05 17:19:06 +0100

    First Commit

---
+++ IndividualCenteredSimulation/Services/MultiAgentSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndividualCenteredSimulation
+{
+    class MultiAgentSystem
+    {
+        public int GridSizeX { get; set; }
+        public int GridSizeY { get; set; }
+
+        public void run() { }
+    }
+}
